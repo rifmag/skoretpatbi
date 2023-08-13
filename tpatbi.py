7473b0324451b184161e52576109809daa81b033
rifmag <142006937+rifmag@users.noreply.github.com>
2023-08-13 19:16:36 +0700

    Update tpatbi.py

--- tpatbi.py
+++ tpatbi.py
@@ -178,7 +178,7 @@ def add_bg_from_url():
          f"""
          <style>
          .stApp {{
-             background-image: url("https://cdn.pixabay.com/photo/2019/04/24/11/27/flowers-4151900_960_720.jpg");
+             background-image: url("https://cdn.pixabay.com/photo/2016/10/11/21/43/geometric-1732847_640.jpg");
              background-attachment: fixed;
              background-size: cover
          }}
